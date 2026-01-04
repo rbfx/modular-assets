5a4f3ca2c9c5f5b3d6ea7b2d82aa693b6eda0d33
Gleb <gleb@gleblebedev.com>
2026-01-04 17:12:37 +0000

    Wall and floor occluders

--- regenerate.bat
+++ regenerate.bat
@@ -2,6 +2,8 @@ dotnet build -c Release "tools/GltfTool"
 dotnet "tools/GltfTool/bin/Release/net9.0/GltfTool.dll" convert -i "src/Env/BrickFloor01.glb" --align 0.5,1.0,0.5 -o "rbfx/Data/Models/Env/BrickFloor01.mdl"
 dotnet "tools/GltfTool/bin/Release/net9.0/GltfTool.dll" convert -i "src/Env/BrickWall01.glb" --align 0.5,0.0,0.5 -o "rbfx/Data/Models/Env/BrickWall01.mdl"
 dotnet "tools/GltfTool/bin/Release/net9.0/GltfTool.dll" convert -i "src/Env/BrickWallArch01.glb" -t -n --align 0.5,0.0,0.5 -o "rbfx/Data/Models/Env/BrickWallArch01.mdl"
+dotnet "tools/GltfTool/bin/Release/net9.0/GltfTool.dll" convert -i "src/Env/FloorOccluder.glb" -r 90,0,0 --align 0.5,0.5,0.5 -o "rbfx/Data/Models/Env/FloorOccluder.mdl"
+dotnet "tools/GltfTool/bin/Release/net9.0/GltfTool.dll" convert -i "src/Env/WallOccluder.glb" --align 0.5,0.0,0.5 -o "rbfx/Data/Models/Env/WallOccluder.mdl"
 
 
 
